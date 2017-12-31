5309fa92792cc18133fc4c5429df84d7f547cda9
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2017-12-31 15:57:48 -0500

    test deply

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({hi: "againe app"})
+  res.send({hi: "deploy app to heroku"})
 })
 
 const PORT = process.env.PORT || 5000
