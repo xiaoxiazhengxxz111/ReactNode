7ba1418997b99962c2ba95ad1b6b29fdcc9aadc5
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2017-12-31 13:41:03 -0500

    fisrt commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({hi: "againe app"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('listen to port 5000...')
+})
