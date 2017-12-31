f700a6e041caaf052f4ca1731d42f14d9d6f7767
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2017-12-31 16:07:49 -0500

    test auto againe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({hi: "deploy app to heroku, test auto"})
+  res.send({hi: "deploy app to heroku, test auto againe"})
 })
 
 const PORT = process.env.PORT || 5000
