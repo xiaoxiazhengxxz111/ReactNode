d002d85198c3d0eb9f3c043512b38138abe01a8e
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2017-12-31 18:19:56 -0500

    add google auth and strategy

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({hi: "deploy app to heroku, test auto againe"})
-})
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accesToken, refreshToken, profile, done) => {
+    console.log('accesToken',accesToken)
+    console.log('refresh Token', refreshToken)
+    console.log('profile', profile)
+}))
+
+app.get('/auth/google', 
+    passport.authenticate('google', 
+    {scope: ['profile', 'email']}
+))
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
