c5dc158a8428c63371f85008cfb6781618ace5a1
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2018-01-18 11:13:15 -0500

    setup index.js and app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+
+const store = createStore(() => [], {}, applyMiddleware())
+
+ReactDOM.render(
+  // provider has store attribut, when store update, it will informe its child component app to update and rerender
+  <Provider store={store}><App /></Provider>, 
+  document.querySelector('#root')
+)
